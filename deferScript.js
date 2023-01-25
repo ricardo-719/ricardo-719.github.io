9ba5cbc8c0a2e8324720f73ce352cf0b44a2b3f6
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-25 19:30:48 +0700

    Update toggle color swap on light mode

--- deferScript.js
+++ deferScript.js
@@ -16,24 +16,33 @@ let toggleLightmodeOne = document.getElementById("toggleLightmodeOne");
 let toggleLightmodeTwo =document.getElementById("toggleLightmodeTwo");
 let toggleDarkmodeOne = document.getElementById("toggleDarkmodeOne");
 let toggleDarkmodeTwo = document.getElementById("toggleDarkmodeTwo");
+let buttonNav = document.getElementsByClassName("button-nav")
+let burgerLine = document.getElementsByClassName("line")
 const viewMode = (screen) => {
     document.body.classList.toggle('lightmode')
     if (toggleState === true) {
-        toggleState = false
+        toggleState = false;
         logo.style.display = 'none';
         toggleDarkmodeOne.style.display = 'none';
         toggleDarkmodeTwo.style.display = 'none';
         logoLightmode.style.display = 'block';
         toggleLightmodeOne.style.display = 'block';
         toggleLightmodeTwo.style.display = 'block';
+        console.log(buttonNav);
+        Array.from(buttonNav).map(function(button) {
+            button.style.border="2px solid #181818";
+        });
     } else {
-        toggleState = true
+        toggleState = true;
         logo.style.display = 'block';
         toggleDarkmodeOne.style.display = 'block';
         toggleDarkmodeTwo.style.display = 'block';
         logoLightmode.style.display = 'none';
         toggleLightmodeOne.style.display = 'none';
         toggleLightmodeTwo.style.display = 'none';
+        Array.from(buttonNav).map(function(button) {
+            button.style.border="2px solid #bcbbff";
+        });
     }
     if (screen === 'mobile') {
         toggleDarkmodeTwo.style.display = 'none';
