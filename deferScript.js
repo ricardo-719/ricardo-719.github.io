8e2a81ea38d139fc6db1abb56ec40c6bc05dd491
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-25 16:31:28 +0700

    Partially implement dark-light mode

--- deferScript.js
+++ deferScript.js
@@ -7,3 +7,39 @@ function active() {
         return [hBN.ariaPressed = "false", hMN.ariaPressed = "false"];
     }
 }
+
+let toggleState = true;
+let toggleMode = document.querySelector('.toggle');
+let logo = document.getElementById("logo");
+let logoLightmode = document.getElementById("logoLightmode");
+let toggleLightmodeOne = document.getElementById("toggleLightmodeOne");
+let toggleLightmodeTwo =document.getElementById("toggleLightmodeTwo");
+let toggleDarkmodeOne = document.getElementById("toggleDarkmodeOne");
+let toggleDarkmodeTwo = document.getElementById("toggleDarkmodeTwo");
+const viewMode = (screen) => {
+    document.body.classList.toggle('lightmode')
+    if (toggleState === true) {
+        toggleState = false
+        logo.style.display = 'none';
+        toggleDarkmodeOne.style.display = 'none';
+        toggleDarkmodeTwo.style.display = 'none';
+        logoLightmode.style.display = 'block';
+        toggleLightmodeOne.style.display = 'block';
+        toggleLightmodeTwo.style.display = 'block';
+    } else {
+        toggleState = true
+        logo.style.display = 'block';
+        toggleDarkmodeOne.style.display = 'block';
+        toggleDarkmodeTwo.style.display = 'block';
+        logoLightmode.style.display = 'none';
+        toggleLightmodeOne.style.display = 'none';
+        toggleLightmodeTwo.style.display = 'none';
+    }
+    if (screen === 'mobile') {
+        toggleDarkmodeTwo.style.display = 'none';
+        toggleLightmodeTwo.style.display = 'none';
+    } else {
+        toggleDarkmodeOne.style.display = 'none';
+        toggleLightmodeOne.style.display = 'none';
+    }
+}
