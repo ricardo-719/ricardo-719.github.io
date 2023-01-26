a8da65dc0e96fc36045523b3b93e9d6b0d75396a
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-26 18:39:37 +0700

    Partially optimize Dark/Light mode code

--- deferScript.js
+++ deferScript.js
@@ -8,17 +8,17 @@ function active() {
     }
 }
 
-let toggleState = true;
+let toggleState = true; //this
 let toggleMode = document.querySelector('.toggle');
-let logo = document.getElementById("logo");
+let logo = document.getElementById("logo"); //this
 let logoLightmode = document.getElementById("logoLightmode");
 let toggleLightmodeOne = document.getElementById("toggleLightmodeOne");
 let toggleLightmodeTwo =document.getElementById("toggleLightmodeTwo");
-let toggleDarkmodeOne = document.getElementById("toggleDarkmodeOne");
+let toggleDarkmodeOne = document.getElementById("toggleDarkmodeOne"); //this
 let toggleDarkmodeTwo = document.getElementById("toggleDarkmodeTwo");
-let buttonNav = document.getElementsByClassName("button-nav")
+let buttonNav = document.getElementById("hamburgerButton")
 let burgerLine = document.getElementsByClassName("line")
-const viewMode = (screen) => {
+/*const viewMode = (screen) => {
     document.body.classList.toggle('lightmode')
     if (toggleState === true) {
         toggleState = false;
@@ -26,8 +26,8 @@ const viewMode = (screen) => {
         toggleDarkmodeOne.style.display = 'none';
         toggleDarkmodeTwo.style.display = 'none';
         logoLightmode.style.display = 'block';
-        toggleLightmodeOne.style.display = 'block';
-        toggleLightmodeTwo.style.display = 'block';
+        toggleLightmodeOne.style.display = 'inline';
+        toggleLightmodeTwo.style.display = 'inline';
         console.log(buttonNav);
         Array.from(buttonNav).map(function(button) {
             button.style.border="2px solid #181818";
@@ -35,8 +35,8 @@ const viewMode = (screen) => {
     } else {
         toggleState = true;
         logo.style.display = 'block';
-        toggleDarkmodeOne.style.display = 'block';
-        toggleDarkmodeTwo.style.display = 'block';
+        toggleDarkmodeOne.style.display = 'inline';
+        toggleDarkmodeTwo.style.display = 'inline';
         logoLightmode.style.display = 'none';
         toggleLightmodeOne.style.display = 'none';
         toggleLightmodeTwo.style.display = 'none';
@@ -51,4 +51,25 @@ const viewMode = (screen) => {
         toggleDarkmodeOne.style.display = 'none';
         toggleLightmodeOne.style.display = 'none';
     }
-}
+}*/
+    // Darkmode Function
+    const viewModeTwo = () => {
+        // This class contains general Dark/Light mode properties
+        document.body.classList.toggle('bodyLightmode');
+        if (toggleState) {
+            toggleState = false;
+            toggleDarkmodeOne.innerHTML = '<i class="fa-solid fa-moon"></i><p>Dark Mode</p>';
+            logo.src = 'https://escrituras-eremitas.com/wp-content/uploads/2023/01/CS50HomepageLogoLightMode.png';
+            buttonNav.setAttribute('fill', 'var(#181818)');
+        } else {
+            toggleState = true;
+            toggleDarkmodeOne.innerHTML = '<i class="fa-regular fa-sun"></i><p>Light Mode</p>';
+            logo.src = 'https://escrituras-eremitas.com/wp-content/uploads/2023/01/CS50HomepageLogo.png';
+            buttonNav.setAttribute('fill', 'var(--button-color)');
+        }
+        
+    }
+    // Dark/Light mode anchor for mobile
+    const viewModeAnchorMobile = document.getElementById('viewModeAnchorMobile');
+    //Event handler
+    viewModeAnchorMobile.onclick = viewModeTwo;
