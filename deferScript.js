6ab83f4ca30936eadfa56e12854c39970d7c222b
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-31 11:17:37 +0700

    Add slides to About Me page

--- deferScript.js
+++ deferScript.js
@@ -41,6 +41,39 @@ const viewMode = () => {
 // Dark/Light mode anchors for mobile & full screen
 const viewModeAnchorMobile = document.getElementById('viewModeAnchorMobile');
 const viewModeAnchorFull = document.getElementById('viewModeAnchorFullScreen')
-//Event handlers
+// Event handlers
 viewModeAnchorMobile.onclick = viewMode;
 viewModeAnchorFull.onclick = viewMode;
+
+// About Me section slices
+const aboutMeImg = document.getElementById('imgSplash');
+const aboutMeText = document.getElementById('subDes');
+const aboutMeButton = document.getElementById('introBtn')
+let currentSlideIndx = 1;
+const aboutMeImgArray = [
+    "https://escrituras-eremitas.com/wp-content/uploads/2022/11/pic1.jpg",
+    "https://escrituras-eremitas.com/wp-content/uploads/2022/11/pic2.jpeg",
+    "https://escrituras-eremitas.com/wp-content/uploads/2022/11/pic3.jpg",
+    "https://escrituras-eremitas.com/wp-content/uploads/2022/11/pic4.jpg",
+    "https://escrituras-eremitas.com/wp-content/uploads/2022/11/pic5.jpg"
+];
+const aboutMeTextArray = [
+    "Welcome to my homepage! Here you will learn more about myself and my programming journey. I will share my background, hobbies and even some of my most notable projects. Enjoy!",
+    "I have a degree in Mechanical Engineering. After four years of work experience I decided to move to Thailand to learn more about Buddhism and to experience a different culture.",
+    "I am originally from the Dominican Republic, a place I love and visit often. My wife is Thai, she is a Social Studies teacher, and as you can see, that's my dad! The greatest guy I know.",
+    "I consider myself to be a Buddhist. I enjoy reading about it and practicing sitting and walking meditation. Every once in a while my wife and I support our local monastery with supplies!",
+    "I'm also a teacher with six years of experience. Recently, I moved from teaching full-time to doing it part-time; now I have more time to continue to develop my programming skills!"
+];
+
+// Function for changing slices
+const aboutMeChangeSlide = () => {
+    if(currentSlideIndx >= aboutMeImgArray.length) {
+        currentSlideIndx = 0;
+    }
+    aboutMeImg.src = aboutMeImgArray[currentSlideIndx];
+    aboutMeText.innerHTML = aboutMeTextArray[currentSlideIndx];
+    aboutMeButton.innerHTML =  `Next [${currentSlideIndx + 1}/5]`;
+    currentSlideIndx++;
+}
+
+aboutMeButton.onclick = aboutMeChangeSlide;
